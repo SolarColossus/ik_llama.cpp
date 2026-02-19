6c8921bd5df9a268d6d3514b41e59b4629b868c0
SolarColossus <zakbrowser123@gmail.com>
2026-02-19 10:56:16 -0500

    Update solve_tri.cu

--- ggml/src/ggml-cuda/solve_tri.cu
+++ ggml/src/ggml-cuda/solve_tri.cu
@@ -777,8 +777,7 @@ static void solve_tri_f32_cuda(const float * A,
         static bool smem_configured_tiled = false;
         if (!smem_configured_tiled) {
 #ifdef GGML_USE_HIPBLAS
-            const void (*solve_256x256_ref) = &solve_tri_f32_256x256_tiled;
-            hipFuncSetAttribute(solve_256x256_ref, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
+            hipFuncSetAttribute((const&)solve_tri_f32_256x256_tiled, cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
 #else
             cudaFuncSetAttribute(solve_tri_f32_256x256_tiled,
                 cudaFuncAttributeMaxDynamicSharedMemorySize, smem_size);
